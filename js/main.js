e4bdaf75b5abff53add982e8244b6b3da36ca117
Pontus Blomqvist <68470513+Ekoda@users.noreply.github.com>
2022-01-13 15:36:23 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-// The Main JS file
+// Hide and Show OpenForum
+$("#hideopenforum").on("click", function(){
+    if ( $('#hideopenforum').attr('class') == 'fas fa-chevron-right' ){
+        $('#hideopenforum').removeClass('fa-chevron-right').addClass('fa-chevron-left')
+        $('#chat').css('display', 'none')
+        $('#web').css('width', '100%')
+    } else if ( $('#hideopenforum').attr('class') == 'fas fa-chevron-left' ){
+        $('#hideopenforum').removeClass('fa-chevron-left').addClass('fa-chevron-right')
+        $('#chat').css('display', 'block')
+        $('#web').css('width', '80%')
+    }
+});
