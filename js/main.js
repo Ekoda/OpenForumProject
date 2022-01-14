cd7f123f646f4cb2087f4689b7cac06b06c64e50
Pontus Blomqvist <68470513+Ekoda@users.noreply.github.com>
2022-01-14 22:22:56 +0100

    Mobile functionality (#6)
    * Added mobile compatability
    
    * Removed unecessary styling

--- js/main.js
+++ js/main.js
@@ -4,10 +4,16 @@ $("#hideopenforum").on("click", function(){
         $('#hideopenforum').removeClass('fa-chevron-right').addClass('fa-chevron-left')
         $('#chat').css('display', 'none')
         $('#web').css('width', '100%')
+        $('#web').css('display', 'block')
     } else if ( $('#hideopenforum').attr('class') == 'fas fa-chevron-left' ){
         $('#hideopenforum').removeClass('fa-chevron-left').addClass('fa-chevron-right')
         $('#chat').css('display', 'block')
         $('#web').css('width', '80%')
+        
+        let screen = $(window)
+        if (screen.width() < 500) {
+            $('#web').css('display', 'none')
+        }
     }
 });
 
