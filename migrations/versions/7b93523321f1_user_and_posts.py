e4b83681f89990e3bd620a9da3d0d9d5ad56633f
Pontus Blomqvist <pontus.blomqvist94@gmail.com>
2022-03-28 16:38:00 +0900

    v1

--- migrations/versions/7b93523321f1_user_and_posts.py
+++ migrations/versions/7b93523321f1_user_and_posts.py
@@ -1,8 +1,8 @@
 """user and posts
 
-Revision ID: 2ccdc9d987c7
+Revision ID: 7b93523321f1
 Revises: 
-Create Date: 2022-03-28 16:18:10.351726
+Create Date: 2022-03-28 16:27:44.750377
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '2ccdc9d987c7'
+revision = '7b93523321f1'
 down_revision = None
 branch_labels = None
 depends_on = None
