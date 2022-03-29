d20b204f95b2670f1fce0d66f4299e6bb072c907
Pontus Blomqvist <pontus.blomqvist94@gmail.com>
2022-03-29 14:14:22 +0900

    logins v0.1

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,7 +9,13 @@ class User(db.Model):
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     image = db.Column(db.String(64)) # Link to user image
-    color = db.Column(db.String(64)) # User color
+    color = db.Column(db.String(64)) # User Hex color
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User{}>'.format(self.username)
@@ -19,3 +26,6 @@ class Post(db.Model):
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     score = db.Column(db.Integer, index=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
