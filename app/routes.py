aeb431852f068a9c4366360177ad76812557ddc7
Pontus Blomqvist <pontus.blomqvist94@gmail.com>
2022-03-29 16:21:38 +0900

    logins v0.23

--- app/routes.py
+++ app/routes.py
@@ -80,9 +80,9 @@ def index():
 
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
+        user = User.query.filter_by(email=form.email.data).first()
         if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
+            flash('Invalid email or password')
             return redirect(url_for('index'))
         login_user(user)
         return redirect(url_for('index'))
