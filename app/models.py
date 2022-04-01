ccb6fbf491d5c10eafe5682e5c7002d8488493d1
Pontus Blomqvist <pontus.blomqvist94@gmail.com>
2022-04-01 15:36:42 +0900

    create acc api

--- app/models.py
+++ app/models.py
@@ -75,7 +75,6 @@ class PostResponse(db.Model):
 class UserNotifications(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    from_user_id = user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
     def __repr__(self):
