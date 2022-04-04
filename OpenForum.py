facc14c2872454f359414b9d4322109ca06164a0
Pontus Blomqvist <pontus.blomqvist94@gmail.com>
2022-04-04 18:37:08 +0900

    Major Blueprint Restructure v0.92

--- OpenForum.py
+++ OpenForum.py
@@ -1,6 +1,8 @@
 from app import create_app, db
 from app.models import PostResponse, User, Post, UserNotifications
 
+app = create_app()
+
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post, 'PostResponse': PostResponse, 'UserNotification': UserNotifications}
