2ed9f71ea0f8cdbf2305b05f5465c9e9b02148aa
Pontus Blomqvist <68470513+Ekoda@users.noreply.github.com>
2022-05-29 13:02:29 +0200

    improved API error handling (#37)

--- app/api/posts.py
+++ app/api/posts.py
@@ -2,14 +2,16 @@ from app import db
 from app.api import bp
 from flask import jsonify, request
 from app.models import User, Post, PostResponse
-from app.api.errors import bad_request
+from app.api.errors import bad_request, request_not_found
 
 @bp.route('/posts/<thread_hash>', methods=['GET'])
 def get_posts(thread_hash):
     data = {
         'posts': [post.to_dict() for post in Post.query.filter_by(thread=thread_hash).all()]
     }
-    return jsonify(data)
+    if len(data['posts']) > 0:
+        return jsonify(data)
+    return request_not_found('No post data on thread: ' + thread_hash)
 
 @bp.route('/post', methods=['POST'])
 def post(data):
