235d00911327c514fd524c7a317647a1b2947793
Pontus Blomqvist <pontus.blomqvist94@gmail.com>
2022-06-06 20:26:39 +0200

    Minor cleanup

--- app/static/js/thread.js
+++ app/static/js/thread.js
@@ -13,7 +13,6 @@ class Thread extends React.Component {
         fetch(url)
         .then(response => response.json())
         .then(data => {
-            
             data.posts.map(post => {
                 post['respond_data'] = { 
                     display: "none", 
